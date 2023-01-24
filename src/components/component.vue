3acb9317e43c3ac2de5d62d2fbf35fcf4828a707
JustKooba <109106079+JustKooba@users.noreply.github.com>
2023-01-24 11:54:45 +0100

    Update component.vue

--- src/components/component.vue
+++ src/components/component.vue
@@ -1,17 +1,37 @@
 <template>
   <div class="container">
     <h1>Joke Gnenerator</h1>
-    <p class="joke">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam obcaecati
-      fugiat consequatur sequi aperiam eius. Nesciunt nisi consectetur impedit
-      sunt at nostrum alias odio nobis, quis ratione itaque voluptate iste!
-    </p>
-    <button>Next Joke</button>
+    <p class="joke">{{ joke }}</p>
+    <button @click="getJoke">Next Joke</button>
   </div>
 </template>
+
 <script>
+import axios from "axios";
+
 export default {
   name: "component",
+  data() {
+    return {
+      joke: "",
+    };
+  },
+  methods: {
+    async getJoke() {
+      try {
+        const response = await axios.get("https://icanhazdadjoke.com", {
+          headers: { Accept: "application/json" },
+        });
+        this.joke = response.data.joke;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+  created() {
+    this.getJoke();
+  },
 };
 </script>
+
 <style></style>
